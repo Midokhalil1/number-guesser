30d9f9056f77809928d77bfdd42c7521f05fbb92
Midokhalil1 <midokhalil22@gmail.com>
2022-10-13 10:38:25 -0400

    guessing

--- guess.js
+++ guess.js
@@ -6,3 +6,38 @@ if it lower say "too low"  and ask again.
 if its then right number say "congratulation" and exit
 
 */
+
+import readline from "readline-sync"
+
+let userGuessedRight = false
+//while(true) {
+// guess a number from 0-10
+let Numberchosen = Math.ceil(Math.random() * 10)
+//console.log(Numberchosen)
+
+
+while (!userGuessedRight) {
+    let guess = readline.question("what is your guess?")
+    if (guess == Numberchosen) {
+        //console.log(guess)
+        console.log("congratulations")
+        userGuessedRight = true
+    }
+        if (guess > Numberchosen) {
+            console.log("too high!")
+            
+
+        }
+        if (guess < Numberchosen) {
+            console.log("too low!")
+        }
+    
+        // console.log("congratulations")
+        // userGuessedRight = true 
+}
+    //}
+
+    // while(!userGuessedRight){
+    //     console.log("what is your guess?")
+    // }
+
