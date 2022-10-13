c52dffcfbd9bc2f47e6b798568e630408f8232ed
Midokhalil1 <midokhalil22@gmail.com>
2022-10-13 09:35:37 -0400

    second

---
+++ guess.js
@@ -0,0 +1,8 @@
+/*
+create a program thats guessses a number from 1 to 10
+then asks us to gues that number.
+if it higher say "too high" and ask again.
+if it lower say "too low"  and ask again.
+if its then right number say "congratulation" and exit
+
+*/
